b948d3fee97fdb5c43e95d360d1bb5dcaab1f323
Deborah Lyra <deborahp.lyra@gmail.com>
2025-02-07 15:11:39 -0300

    added whatsapp btn

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -4,6 +4,7 @@ import { Contact } from "../components/contact/Contact";
 import { Footer } from "../components/footer/Footer";
 import { Navbar } from "../components/Navbar/Navbar";
 import { Surgeries } from "../components/surgeries/Surgeries";
+import { WhatsappLogo } from "phosphor-react";
 
 
 export function HomePage() {
@@ -12,9 +13,17 @@ export function HomePage() {
       <Navbar />
       <Banner />
       <AboutMe />
-      <Surgeries/>
+      <Surgeries />
       <Contact />
-      <Footer/>
+      <Footer />
+      <a
+        className="fixed bottom-8 right-8 flex items-center justify-center bg-deepBlue text-white hover:bg-skyBlue rounded-full shadow-lg p-4 transition-all duration-300 transform hover:scale-110 hover:shadow-xl"
+        aria-label="Contato via WhatsApp"
+        href="https://api.whatsapp.com/send?phone=5581993169020&text=Ol%C3%A1,%20gostaria%20de%20agendar%20uma%20consulta%20com%20Dr.%20Járys%20Borba%20"
+        target="_blank"
+      >
+        <WhatsappLogo size={32} />
+      </a>
     </div>
   )
 }
