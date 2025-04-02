a8456aaed40c2d2a50e8afb963971bba5cfe5293
Deborah Lyra <deborahp.lyra@gmail.com>
2025-04-01 21:23:18 -0300

    added google tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-
-import Router from "./Router"
-
+import { Helmet } from "react-helmet";
+import Router from "./Router";
 
 function App() {
- 
-
   return (
     <>
-      <Router/>
-   
+      {/* Google Tag Manager no HEAD */}
+      <Helmet>
+        <script>
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-PVBDZDCF');`}
+        </script>
+      </Helmet>
+
+      {/* Google Tag Manager no BODY */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-PVBDZDCF"
+          height="0"
+          width="0"
+          style={{ display: "none", visibility: "hidden" }}
+        ></iframe>
+      </noscript>
+
+      {/* Carrega as rotas da aplicação */}
+      <Router />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
